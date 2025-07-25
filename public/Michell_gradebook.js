608ff9fa05ffc5036174dbb22bccf47bd23bf99a
Updated-to-Pro <108355921+Updated-to-Pro@users.noreply.github.com>
2025-07-25 03:03:59 -0400

    Update Michell_gradebook.js

--- public/Michell_gradebook.js
+++ public/Michell_gradebook.js
@@ -1,1 +1,8 @@
-
+fucntion fetchGrandeData(){
+  console.log("Fetching grade data....")
+}
+fucntion populateGradebook(data){
+  console.log("Populating gradebook with data:", data)
+}
+const gradeData = fetchGradeData();
+populateGradebook(gradeData);
